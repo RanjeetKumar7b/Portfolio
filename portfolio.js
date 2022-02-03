f8232e4a1b8ce2ff664256f1d44a55643f929853
Ranjeet Kumar <77196517+RanjeetKumar7b@users.noreply.github.com>
2022-02-03 15:42:15 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Learner"," Creater","web Developer","web Designer"],
+        strings: ["Learner","Coder","Developer","Web Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
